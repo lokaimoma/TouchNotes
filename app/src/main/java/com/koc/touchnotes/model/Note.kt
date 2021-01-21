19c471558b8989241e33379010c2005d12f27373
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2021-01-21 22:23:55 +0100

    Annotate Note class with kotlinx-Parcelable

--- app/src/main/java/com/koc/touchnotes/model/Note.kt
+++ app/src/main/java/com/koc/touchnotes/model/Note.kt
@@ -1,15 +1,18 @@
 package com.koc.touchnotes.model
 
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 /**
 Created by kelvin_clark on 12/5/2020
  */
+@Parcelize
 @Entity
 data class Note(
     var title : String? = "New Note",
     var body : String? = "",
     @PrimaryKey(autoGenerate = true) val id :Int = 0
-)
+) : Parcelable
