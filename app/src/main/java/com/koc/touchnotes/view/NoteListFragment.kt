4f3180a4a1e4ef6d398c41239a6c0ca5c7494190
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2021-01-29 00:19:34 +0100

    Add event to handle click events

--- app/src/main/java/com/koc/touchnotes/view/NoteListFragment.kt
+++ app/src/main/java/com/koc/touchnotes/view/NoteListFragment.kt
@@ -5,27 +5,32 @@ import android.view.*
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.koc.touchnotes.R
 import com.koc.touchnotes.databinding.FragmentNoteListBinding
 import com.koc.touchnotes.enums.NoteSort
+import com.koc.touchnotes.interfaces.ClickListener
+import com.koc.touchnotes.model.Note
+import com.koc.touchnotes.util.NoteEvent
+import com.koc.touchnotes.util.exhaustive
 import com.koc.touchnotes.view.extensions.queryTextListener
 import com.koc.touchnotes.viewModel.NoteListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import javax.inject.Inject
+import kotlinx.coroutines.flow.collect
 
 /**
  * Created by kelvin_clark on 5/12/20
  */
 @AndroidEntryPoint
-class NoteListFragment : Fragment() {
+class NoteListFragment : Fragment(), ClickListener {
     private var _binding: FragmentNoteListBinding? = null
     private val binding get() = _binding!!
 
-    @Inject
-    lateinit var notesAdapter: NotesRecyclerAdapter
+    var notesAdapter = NotesRecyclerAdapter(this)
 
     private val noteListViewModel: NoteListViewModel by viewModels()
 
@@ -48,12 +53,26 @@ class NoteListFragment : Fragment() {
             itemsNotes.layoutManager = GridLayoutManager(context, 2,
                 GridLayoutManager.VERTICAL, false)
             fabAdd.setOnClickListener {
-                val action = NoteListFragmentDirections.actionListEdit()
-                Navigation.findNavController(it).navigate(action)
+                noteListViewModel.addNoteClicked()
             }
         }
 
         observeNoteList()
+        collectFlows()
+    }
+
+    private fun collectFlows() = viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+        noteListViewModel.noteEvent.collect {event ->
+            when(event){
+                is NoteEvent.NoteClickedEvent -> {
+                    val action = NoteListFragmentDirections.actionListEdit(event.note)
+                    findNavController().navigate(action)
+                }
+                is NoteEvent.AddNoteEvent -> {
+                    findNavController().navigate(R.id.action_list_edit)
+                }
+            }.exhaustive
+        }
     }
 
     @ExperimentalCoroutinesApi
@@ -97,5 +116,9 @@ class NoteListFragment : Fragment() {
         super.onDestroy()
         _binding = null
         notesAdapter.clearUp()
+    }
+
+    override fun onClickListener(note: Note) {
+        noteListViewModel.noteClicked(note)
     }
 }
