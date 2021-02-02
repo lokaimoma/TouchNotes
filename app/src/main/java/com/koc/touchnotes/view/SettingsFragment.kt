f04ffae5511d144c2c7f261579f53a70941c41f8
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2021-02-02 22:24:54 +0100

    Add functionality to change theme from settings screen

--- app/src/main/java/com/koc/touchnotes/view/SettingsFragment.kt
+++ app/src/main/java/com/koc/touchnotes/view/SettingsFragment.kt
@@ -1,12 +1,42 @@
 package com.koc.touchnotes.view
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import com.koc.touchnotes.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class SettingsFragment : PreferenceFragmentCompat() {
+@AndroidEntryPoint
+class SettingsFragment : PreferenceFragmentCompat(){
 
+    lateinit var listener: SharedPreferences.OnSharedPreferenceChangeListener
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listener = SharedPreferences.OnSharedPreferenceChangeListener {sharedPreferences, key ->
+            if (key == getString(R.string.theme_key)){
+                when (sharedPreferences?.getString(key, "")) {
+                    getString(R.string.system_default_theme) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    getString(R.string.light_theme) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    getString(R.string.dark_theme) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener)
     }
 }
