1fc02f8a367f7ec6c69bc219b0333cce7c17a2c1
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2021-02-19 15:13:34 +0100

    Complete test for note database operations

--- app/src/androidTest/java/com/koc/touchnotes/model/NoteDatabaseTest.kt
+++ app/src/androidTest/java/com/koc/touchnotes/model/NoteDatabaseTest.kt
@@ -1,13 +1,18 @@
 package com.koc.touchnotes.model
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.asLiveData
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import com.koc.touchnotes.enums.NoteSort
+import com.koc.touchnotes.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,6 +23,9 @@ Created by kelvin_clark on 2/19/2021 2:41 PM
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class NoteDatabaseTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var noteDatabase: NoteDatabase
     private lateinit var noteDao: NotesDao
@@ -48,5 +56,41 @@ class NoteDatabaseTest {
 
         val retrievedNote = noteDao.getNote(note.id)
         assertThat(retrievedNote).isEqualTo(note)
+    }
+
+    @Test
+    fun deleteIteFromDatabase() = runBlockingTest {
+        val note = Note(
+            "New note", "Note content",
+            System.currentTimeMillis(), System.currentTimeMillis(), 1
+        )
+
+        noteDao.insertNote(note)
+        noteDao.removeNote(note.id)
+
+        val retrievedNotes = noteDao.getNotes("", NoteSort.BY_CREATED_TIME)
+            .asLiveData().getOrAwaitValue()
+
+        assertThat(retrievedNotes).doesNotContain(note)
+    }
+
+    @Test
+    fun updateItemInDatabase() = runBlockingTest {
+        val note = Note(
+            "New note", "Note content",
+            System.currentTimeMillis(), System.currentTimeMillis(), 1
+        )
+
+        noteDao.insertNote(note)
+
+        val noteUpdated = Note(
+            "New note", "Updated body",
+            System.currentTimeMillis(), System.currentTimeMillis(), 1
+        )
+
+        noteDao.updateNote(noteUpdated)
+
+        val retrievedNote = noteDao.getNote(note.id)
+        assertThat(retrievedNote).isEqualTo(noteUpdated)
     }
 }
